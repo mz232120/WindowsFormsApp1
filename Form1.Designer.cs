c6c21ddf0b93ec49bb9dfa4de7e5f5deb8a2b9ac
25386 <25386@MI_G>
2026-06-27 17:24:22 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblpwd = new System.Windows.Forms.Label();
+            this.lblMsg1 = new System.Windows.Forms.Label();
+            this.lblMsg2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(129, 121);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(62, 18);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "账户名";
+            // 
+            // lblpwd
+            // 
+            this.lblpwd.AutoSize = true;
+            this.lblpwd.Location = new System.Drawing.Point(132, 199);
+            this.lblpwd.Name = "lblpwd";
+            this.lblpwd.Size = new System.Drawing.Size(53, 18);
+            this.lblpwd.TabIndex = 1;
+            this.lblpwd.Text = "密 码";
+            // 
+            // lblMsg1
+            // 
+            this.lblMsg1.AutoSize = true;
+            this.lblMsg1.ForeColor = System.Drawing.Color.Red;
+            this.lblMsg1.Location = new System.Drawing.Point(424, 121);
+            this.lblMsg1.Name = "lblMsg1";
+            this.lblMsg1.Size = new System.Drawing.Size(143, 18);
+            this.lblMsg1.TabIndex = 3;
+            this.lblMsg1.Text = "*用户名不能为空";
+            // 
+            // lblMsg2
+            // 
+            this.lblMsg2.AutoSize = true;
+            this.lblMsg2.ForeColor = System.Drawing.Color.Red;
+            this.lblMsg2.Location = new System.Drawing.Point(424, 192);
+            this.lblMsg2.Name = "lblMsg2";
+            this.lblMsg2.Size = new System.Drawing.Size(125, 18);
+            this.lblMsg2.TabIndex = 2;
+            this.lblMsg2.Text = "*密码不能为空";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(217, 111);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(139, 28);
+            this.txtName.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(217, 282);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(83, 36);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(411, 282);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(79, 36);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(217, 189);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(139, 28);
+            this.txtPwd.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 393);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblMsg1);
+            this.Controls.Add(this.lblMsg2);
+            this.Controls.Add(this.lblpwd);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "用户登录";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblpwd;
+        private System.Windows.Forms.Label lblMsg1;
+        private System.Windows.Forms.Label lblMsg2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPwd;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //退出按钮的功能
+            //1.确认退出
+            MessageBox.Show("确认退出(y/n)?", "提示", MessageBoxButtons.YesNo);
+            //2.关闭窗口:当前窗体的对象:this
+            this.Dispose();//dispose：消失  
+            //this.Hide();//不可见:对象还在
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //1.判断用户名和密码是否填写
+            string name = txtName.Text.Trim();
+            string pwd = txtPwd.Text.Trim();
+
+            if (name.Length == 0 || pwd.Length == 0)
+            {
+                MessageBox.Show("请输入用户和密码");
+                return;//程序段结束
+            }
+
+            //2.填写完成后，判断用户名是否为"admin",密码"123"  
+            if (name.Equals("admin") && pwd.Equals("123"))
+            {
+                //2.1 是:提示登录成功,跳转到主界面
+                MessageBox.Show("亲爱的 [" + name + "]用户,欢迎使用甲骨文学员信息管理系统v1.0...");
+                //2.2 跳转到主窗体
+                FormMain formMain = new FormMain();
+                formMain.Show();
+                //2.3 自己隐藏
+                this.Hide();
+
+            }
+            else
+            {
+                //2.1 否:提示登录失败,清空用户名和密码
+                MessageBox.Show("用户名或密码不存在,请重新输入");
+                txtName.Text = "";
+                txtPwd.Text = "";
+            }
+
+        }
+    }
+}
